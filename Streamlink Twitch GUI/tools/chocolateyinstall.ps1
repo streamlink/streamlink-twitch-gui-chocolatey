be88a51f96a64441e4e9552c4d0eb4ef7b4c7848
Bo Osterud <github.com@conjuro.net>
2018-08-02 00:55:59 +0200

    v1.6.0 (#23)

--- Streamlink Twitch GUI/tools/chocolateyinstall.ps1
+++ Streamlink Twitch GUI/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'streamlink-twitch-gui'
-$packageVersion = 'v1.5.0'
+$packageVersion = 'v1.6.0'
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $installDir = Split-Path -parent $toolsDir
 $downloadPath = "https://github.com/streamlink/streamlink-twitch-gui/releases/download/$packageVersion/"
@@ -7,10 +7,10 @@ $downloadPath = "https://github.com/streamlink/streamlink-twitch-gui/releases/do
 Install-ChocolateyZipPackage `
 	-PackageName    $packageName `
 	-Url            "$($downloadPath)streamlink-twitch-gui-$packageVersion-win32.zip" `
-	-Checksum       "105b323fe02e903fe4cc67522bd1f1dc4e17bc0b0a4125cd34d4fdb79fd2d723" `
+	-Checksum       "7854388fe98703cb2102ec845d5ee790f03bb79ef618b95754fdc3d1c87261eb" `
 	-ChecksumType   "sha256" `
 	-Url64bit       "$($downloadPath)streamlink-twitch-gui-$packageVersion-win64.zip" `
-	-Checksum64     "dbf1feab4110c35156465a0b50c416364210d08234056e17ad7dfd6ed7f4d902" `
+	-Checksum64     "805be0b95eb7b794aefa1f32e5737c136289f85a9e25891c915453b168909074" `
 	-ChecksumType64 "sha256" `
 	-UnzipLocation  $installDir
 
