318cdeddaf0cb301013d4f247df8b20956dc30de
Starz0r <starz0r@starz0r.com>
2020-12-29 19:02:02 -0600

    Install directory needs to exist beforehand
    Looks like New-Item is also recursive with directories, so this probably can be simplified.

--- Streamlink Twitch GUI/tools/chocolateyinstall.ps1
+++ Streamlink Twitch GUI/tools/chocolateyinstall.ps1
@@ -4,6 +4,7 @@ $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $installDir = Split-Path -parent $toolsDir
 $downloadPath = "https://github.com/streamlink/streamlink-twitch-gui/releases/download/$packageVersion/"
 
+New-Item -ItemType directory $installDir\$packageName\ -EA 0 | Out-Null
 New-Item -ItemType file $installDir\$packageName\notification_helper.exe.ignore -EA 0 | Out-Null
 New-Item -ItemType directory $installDir\$packageName\bin\ -EA 0 | Out-Null
 New-Item -ItemType directory $installDir\$packageName\bin\win64\ -EA 0 | Out-Null
