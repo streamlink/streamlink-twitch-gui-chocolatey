614745300ef238d7ea8e629451e0bb81732eb018
Scott Walters <me@scowalt.com>
2022-03-01 06:54:06 -0800

    Fix v2.0.0 (#50)
    * Fix v2.0.0
    
    * Fix release notes for now
    
    * Fixed root

--- Streamlink Twitch GUI/tools/chocolateyinstall.ps1
+++ Streamlink Twitch GUI/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 $packageName = 'streamlink-twitch-gui'
-$packageVersion = 'v1.13.0'
+$packageVersion = 'v2.0.0'
 $toolsDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 $installDir = Split-Path -parent $toolsDir
 $downloadPath = "https://github.com/streamlink/streamlink-twitch-gui/releases/download/$packageVersion/"
@@ -18,7 +18,7 @@ Install-ChocolateyZipPackage `
 	-Checksum "ddf865ec7c9885272fb52fb560a16d84328a9594e60211c1dcde1571349541a0" `
 	-ChecksumType   "sha256" `
 	-Url64bit       "$($downloadPath)streamlink-twitch-gui-$packageVersion-win64.zip" `
-	-Checksum64     "ba0e2e6f27ab9e406d160942134d8b482e7659940e39af35ca171ff1e31c6357" `
+	-Checksum64     "75B9CDABEF572CDB3EA61F467443CD9F6C5C463AC87ECF1D0E4DE0123F1D6E5E" `
 	-ChecksumType64 "sha256" `
 	-UnzipLocation  $installDir
 
@@ -26,4 +26,3 @@ $desktop = [Environment]::GetFolderPath("Desktop")
 $shortcutFile = Join-Path $desktop "$packageName.lnk"
 $exeFile = Join-Path (Join-Path $installDir $packageName) "$packageName.exe"
 Install-ChocolateyShortcut -shortcutFilePath $shortcutFile -targetPath $exeFile
-
